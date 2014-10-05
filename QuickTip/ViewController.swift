21fa350a3e3822f6bb57f204b59ef100931895e8
aplescia <anthony.plescia@mail.mcgill.ca>
2014-10-05 00:54:38 -0400

    initial commit

---
+++ QuickTip/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  QuickTip
+//
+//  Created by Anthony Plescia on 2014-10-02.
+//  Copyright (c) 2014 Anthony Plescia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    
+
+    @IBOutlet var theButton : UIButton!
+    
+
+}
+
